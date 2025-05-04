44fd4815088be9577a2e43c994f98e3cf318b3a6
Andreas Kollegger <akollegger@gmail.com>
2025-05-04 18:44:53 +0100

    corrected key used for setting current user

--- graphrag_chat/tools.py
+++ graphrag_chat/tools.py
@@ -8,5 +8,5 @@ def set_user(name:str, tool_context: ToolContext) -> str:
 
 def get_user(tool_context: ToolContext) -> str:
     if "current_user" in tool_context.state:
-        return tool_context.state["current"]
-    return "You haven't introduced yourself. How do you like to be called?"
+        return tool_context.state["current_user"]
+    return "User hasn't been identified. Politely ask them to introduce themselves. If you haven't yet, remind them who you are."
